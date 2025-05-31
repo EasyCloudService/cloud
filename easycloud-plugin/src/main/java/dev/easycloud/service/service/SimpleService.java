44cb3d31b6850e0061e906ba910ae00c763de04a
flxwdns <radik.fr@proton.me>
2025-05-31 23:58:14 +0200

    refactor: add jitpack.yml for OpenJDK 21 support and clean up SimpleService imports

--- easycloud-plugin/src/main/java/dev/easycloud/service/service/SimpleService.java
+++ easycloud-plugin/src/main/java/dev/easycloud/service/service/SimpleService.java
@@ -7,9 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Slf4j
 @Getter
 @AllArgsConstructor
