d6ab33681c08bde58020e41e34ab2bef85cd5cc4
flxwdns <radik.fr@proton.me>
2025-06-07 23:22:38 +0200

    feat: enhance property retrieval method to support multiple data types

--- easycloud-api/src/main/java/dev/easycloud/service/service/Service.java
+++ easycloud-api/src/main/java/dev/easycloud/service/service/Service.java
@@ -29,9 +29,46 @@ public interface Service {
         this.addProperty(property.key(), value);
     }
 
-    default <T> T property(String key, Class<T> ignoredClazz) {
-        return (T) properties().get(key);
+
+    default <T> T property(String key, Class<T> clazz) {
+        var value = String.valueOf(this.properties().get(key));
+        Object result = value;
+        if(clazz.getSimpleName().equalsIgnoreCase("integer")) {
+            result = (int) Double.parseDouble(value);
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("boolean")) {
+            result = Boolean.parseBoolean(value);
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("double")) {
+            result = Double.parseDouble(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("float")) {
+            result = Float.parseFloat(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("long")) {
+            result = Long.parseLong(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("short")) {
+            result = Short.parseShort(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("byte")) {
+            result = Byte.parseByte(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("char")) {
+            result = Character.valueOf(String.valueOf(value).charAt(0));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("class")) {
+            result = Enum.valueOf((Class<Enum>) clazz, String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("path")) {
+            result = Path.of(String.valueOf(value));
+        }
+        if(clazz.getSimpleName().equalsIgnoreCase("uuid")) {
+            result = UUID.fromString(String.valueOf(value));
+        }
+        return (T) result;
     }
+
 
     default <T> T property(Property<T> property) {
         return this.property(property.key(), property.type());
