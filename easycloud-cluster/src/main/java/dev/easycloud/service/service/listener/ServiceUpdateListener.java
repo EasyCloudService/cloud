2ba5d8fc83f179ea748924b7068b9790df62abbd
flxwdns <radik.fr@proton.me>
2025-06-09 17:35:16 +0200

    feat: enhance service update handling by processing and logging previous service state

--- easycloud-cluster/src/main/java/dev/easycloud/service/service/listener/ServiceUpdateListener.java
+++ easycloud-cluster/src/main/java/dev/easycloud/service/service/listener/ServiceUpdateListener.java
@@ -2,6 +2,7 @@ package dev.easycloud.service.service.listener;
 
 import dev.easycloud.service.EasyCloudCluster;
 import dev.easycloud.service.network.event.resources.ServiceUpdateEvent;
+import dev.easycloud.service.service.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -9,8 +10,15 @@ public final class ServiceUpdateListener {
 
     public ServiceUpdateListener() {
         EasyCloudCluster.instance().eventProvider().socket().read(ServiceUpdateEvent.class, (netChannel, event) -> {
+            var service = (ServiceImpl) event.service();
+            var oldService = (ServiceImpl) EasyCloudCluster.instance().serviceProvider().get(service.id());
+
+            service.process(oldService.process());
+            service.logStream(oldService.logStream());
+            service.logCache().addAll(oldService.logCache());
+
             EasyCloudCluster.instance().serviceProvider().services().removeIf(it -> it.id().equals(event.service().id()));
-            EasyCloudCluster.instance().serviceProvider().services().add(event.service());
+            EasyCloudCluster.instance().serviceProvider().services().add(service);
 
             EasyCloudCluster.instance().eventProvider().publish(event);
         });
