80d0a40f234f02de4353fb4aba0c7d91939ed4f4
flxwdns <radik.fr@proton.me>
2025-06-11 22:02:06 +0200

    bump version to 0.9.1-preview1

--- easycloud-loader/src/main/java/dev/easycloud/service/EasyCloudVersion.java
+++ easycloud-loader/src/main/java/dev/easycloud/service/EasyCloudVersion.java
@@ -1,5 +1,5 @@
 package dev.easycloud.service;
 
 public final class EasyCloudVersion {
-    public static final String VERSION = "0.9.0-preview1";
+    public static final String VERSION = "0.9.1-preview1";
 }
